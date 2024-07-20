87a9f381d96ba05b0242d5942b1ca694520082f0
Nelskl <adarshprakash88@gmail.com>
2024-07-20 22:19:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import config
 @bot.event()
 async def on_ready():
     print("Bot Is Ready")
+    await bot.load_extension(cogs.extension)
+    print("Mod extension Loaded")
 
 
 bot.run(config.TOKEN)
